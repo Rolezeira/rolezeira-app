3df78fea52ae8332b6a524bd58e1d8d6a92316ff
Janderson Silva <janderson.rdsilva@gmail.com>
2019-03-16 22:13:37 -0300

    Adding firebase authentication to the project

---
+++ src/environments/firebase.sample.ts
@@ -0,0 +1,8 @@
+export const firebase = {
+    apiKey: '[api-key]',
+    authDomain: '[project-id].firebaseapp.com',
+    databaseURL: 'https://[project-id].firebaseio.com',
+    projectId: '[project-id]',
+    storageBucket: '[project-id].appspot.com',
+    messagingSenderId: '[messaging-sender-id]'
+};
