dfab8367a1eba19c215d420bd16d7add8b3e1a33
Janderson Silva <janderson.rdsilva@gmail.com>
2019-03-19 00:18:39 -0300

    WIP: simple posts retrieve structure

--- src/app/feed/feed.page.ts
+++ src/app/feed/feed.page.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-feed',
   templateUrl: 'feed.page.html',
   styleUrls: ['feed.page.scss']
 })
-export class FeedPage {
-  constructor() {
-    this.test();
+export class FeedPage implements OnInit{
+
+  posts: Observable<any[]>;
+
+  constructor(private fireStore: AngularFirestore) { }
+
+  ngOnInit() {
+    this.posts = this.fireStore.collection<any>('posts').valueChanges();
   }
-  test() {
-    console.log('hey');
-  }
-}
+ }
